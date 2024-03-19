1ca14e95b24fab9fb4c391bdf4977978ff452329
RCLab0 <r.c.lab0857@gmail.com>
2024-03-20 05:18:58 +0900

    changed branch name because alias doesnt work

--- create_article.sh
+++ create_article.sh
@@ -2,7 +2,7 @@
 if [ $# -eq 1 ]; then
     # 他の記事の branch で作業中に実行しても main から switch できるように
     git switch main
-    git switch -c new_article/$1
+    git switch -c feature/$1
     npx zenn new:article --slug $1
 else
     echo "Argument not found. You must spacify the slug of the article."
